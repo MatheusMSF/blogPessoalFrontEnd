de4e88b331b58e4004d1699d4f783491150ffb02
Matheus Ferreira <matheus@java.com>
2022-05-10 15:19:54 -0300

    Implementação do put e delete de postagens

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -19,8 +19,20 @@ export class PostagemService {
     return this.http.get<Postagem[]>('',this.token)
   }
 
+  getByIdPostagem(id:number):Observable<Postagem>{
+    return this.http.get<Postagem>(`local/${id}` , this.token)
+  }
+
   postPostagem(postagem: Postagem) : Observable<Postagem>{
     return this.http.post<Postagem>('', postagem, this.token)
+  }
+
+  putPostagem(postagem: Postagem): Observable<Postagem>{
+    return this.http.put<Postagem>('local',postagem,this.token)
+  }
+
+  deletePostagem(id: number){
+    return this.http.delete(`local/${id}`, this.token)
   }
 
 }
